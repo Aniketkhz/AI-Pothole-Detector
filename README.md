344bda71266828d1e0d5f054f8581f67199b6f62
demo <aniketkhadanga2@gmail.com>
2025-07-09 14:46:54 +0530

    running yolo v8

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
 ## Day 3: Labeling Workflow
 
 1. Setup labeling environment:
 ```bash
 python scripts/label_utils.py
-=======
+
+
 ## Day 2: Image processing
 
 1. processing raw images
---
+++ train.py
@@ -0,0 +1,10 @@
+﻿from ultralytics import YOLO
+
+model = YOLO('yolov8n.pt')
+results = model.train(
+    data=r'/dataset.yaml',
+    epochs=100,
+    imgsz=640,
+    batch=8,
+    device='cpu'  # Change to '0' if you have GPU
+)
